3f56e2360d7742521c3a868527f67e1a46c1baba
Brett g Porter <brett@bgporter.net>
2022-11-25 09:19:19 -0500

    Add usage warning.

--- cello.cpp
+++ cello.cpp
@@ -2,5 +2,15 @@
 // Copyright (c) 2022 Brett g Porter. All Rights Reserved.
 //
 
+#ifdef CELLO_H_INCLUDED
+/* When you add this cpp file to your project, you mustn't include it in a file where
+   you've already included any other headers - just put it inside a file on its own,
+   possibly with your config flags preceding it, but don't include anything else. That
+   also includes avoiding any automatic prefix header files that the compiler may be
+   using.
+*/
+#error "Incorrect use of JUCE cpp file"
+#endif
+
 #include "cello/cello_object.cpp"
 #include "cello/cello_value.cpp"
