1632af2dad21e998ef9c38c1aa106730831f65c4
Brett g Porter <bgp@bgporter.net>
2023-03-18 10:19:27 -0400

    Correct template error in Object::getattr (#12)
    * Add initial query code and tests.
    
    * Adding query tests.
    
    * additional tests -- multi-predicate
    
    * Replace VT calls w Object/Value
    
    * Add sorting.
    
    * Updates before releasing:
    - upsert testing
    - clone()
    - bump version #
    - update README
    
    * remove DBG calls
    
    * Update for 1.1.1 release
    
    - correct template errors.
    
    * Object::save now ensures its file is created
    
    * bump version #
    
    * fix conflict marker
    
    * fix template error in Object::getattr, bump version.
    
    ---------
    
    Co-authored-by: Brett g Porter <brett@bgporter.net>

--- cello.h
+++ cello.h
@@ -24,7 +24,7 @@
 BEGIN_JUCE_MODULE_DECLARATION
  ID:               cello
  vendor:           Brett g Porter
- version:          1.1.2
+ version:          1.1.3
  name:             Cello
  description:      Classes for working with JUCE Value Trees
  website:          https://github.com/bgporter/cello
